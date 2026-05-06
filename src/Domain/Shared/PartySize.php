47b31e64ca1a0851374e31779188a857b989e693
Brian Yang <brianjsyang@gmail.com>
2026-05-05 22:48:00 -0700

    Basic setup for PartySize and TimeSlot value objects

--- src/Domain/Shared/PartySize.php
+++ src/Domain/Shared/PartySize.php
@@ -1,6 +1,29 @@
 <?php
 
+declare(strict_types=1);
 /**
  * PartySize will be set up as value object
  * No need for identity
  */
+final class PartySize
+{
+    public function __construct(private readonly int $partySize)
+    {
+        if ($partySize < 1 || $partySize > 15) {
+            throw new \InvalidArgumentException("Party size must be larger than 1 and less than 15. Inserted {$partySize}");
+        }
+    }
+
+    public function value(): int
+    {
+        return $this->partySize;
+    }
+
+    /**
+     * Equality comparison
+     */
+    public function equals(self $other): bool
+    {
+        return $this->partySize === $other->partySize;
+    }
+}
--- src/Domain/Shared/TimeSlot.php
+++ src/Domain/Shared/TimeSlot.php
@@ -1,7 +1,18 @@
 <?php
 
+declare(strict_types=1);
 /**
  * Time Slot will be set up as value object
  * Any TimeSlot set at "7pm" is the same.
  * No need for identity
  */
+
+final class TimeSlot
+{
+    public function __construct(private readonly DateTimeImmutable $start, private readonly int $durationInMinutes)
+    {
+        if (!$start || $durationInMinutes < 0) {
+            throw new \InvalidArgumentException('Either Start is empty or Duration is not a valid input');
+        }
+    }
+}
